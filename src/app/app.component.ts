5cfe26082c3c20e5299ae14cee544a501aee6d32
Akshay Jain <rj.akshayjain@yahoo.com>
2020-07-06 01:40:24 -0700

    Adds some map markers, changes map type to terrain, and initializes with first marker as center

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,68 @@ import { } from '@agm/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  lat: number = 49.220;
-  lng: number = -122.773;
+  // Initial settings for the map
+  lat: number;
+  lng: number;
   zoom: number = 12;
+  mapType: string = 'terrain';
 
-  constructor() {}
+  constructor() {
+    this.initializeMap(this.markers[0]);
+  }
+
+  // Sets the center of the map
+  initializeMap(data: marker) {
+    this.lat = data.lat;
+    this.lng = data.lng;
+
+  }
+  markers: marker[] = [
+    {
+      animation: 'bounce',
+      draggable: false,
+      lat: 49.220,
+      lng: -122.773,
+      opacity: 1,
+      title: 'Random',
+      label: 'Y'
+    },
+    {
+      draggable: true,
+      lat: 49.204,
+      lng: -122.836,
+      opacity: 0.63,
+      title: 'Bakery'
+    },
+    {
+      animation: 'drop',
+      draggable: true,
+      lat: 49.191,
+      lng: -122.762,
+    },
+    {
+      draggable: true,
+      lat: 49.248,
+      lng: -122.797,
+      opacity: 1,
+      title: 'Store'
+    },
+    {
+      animation: 'drop',
+      draggable: false,
+      label: 'R',
+      lat: 49.213,
+      lng: -122.673
+    },
+  ]; 
+}
+
+interface marker {
+  animation?: string,
+  draggable: boolean, 
+  label?: string,
+  lat: number,
+  lng: number,
+  opacity?: number,
+  title?: string
 }
